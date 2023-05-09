aa0c7ab2ebe6f0b22ba0a6e6337fd38df04b7166
luked177 <43680876+luked177@users.noreply.github.com>
2023-05-09 23:00:04 +0100

    Add Search Logic

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -1,12 +1,48 @@
 "use client";
-import React from "react";
-import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useRef, useState, useTransition } from "react";
 
 export default function Search({}) {
-	const [search, setSearch] = useState("");
+	const ref = useRef();
+	const { replace } = useRouter();
+	const pathname = usePathname();
+	const params = new URLSearchParams(window.location.search);
+	const [searchTerm, setSearchTerm] = useState(params.get("search") || "");
+
+	const handleSearch = (searchValue) => {
+		if (searchValue) {
+			params.set("search", searchValue);
+		} else {
+			params.delete("search");
+		}
+
+		replace(`${pathname}?${params.toString()}`);
+	};
+
 	return (
 		<div>
-			<input className='bg-white rounded focus-visible:outline-none text-black' type='text' onBlur={(e) => setSearch(e.target.value)} />
+			<input
+				ref={ref}
+				className='bg-white rounded focus-visible:outline-none text-black'
+				type='text'
+				onChange={(e) => {
+					setSearchTerm(e.target.value);
+					handleSearch(e.target.value);
+				}}
+				defaultValue={searchTerm}
+			/>
+			{searchTerm.length > 0 && (
+				<button
+					onClick={() => {
+						ref.current.value = null;
+						setSearchTerm("");
+						params.delete("search");
+						replace(`${pathname}?${params.toString()}`);
+					}}
+				>
+					❌
+				</button>
+			)}
 		</div>
 	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import AddPost from "./components/addPost";
 import { getContainer } from "./DB/getContainer";
 import Post from "./components/post";
 
-export default async function Home() {
+export default async function Home({ params, searchParams }) {
 	const container = await getContainer();
-	const posts = (await container.items.query("SELECT * FROM c ORDER BY c.time DESC").fetchAll()).resources;
+	const postsAll = (await container.items.query("SELECT * FROM c ORDER BY c.time DESC").fetchAll()).resources;
+	const postsBySearch = (await container.items.query(`SELECT * FROM c WHERE c.post LIKE '%${searchParams?.search}%' ORDER BY c.time DESC`).fetchAll()).resources;
 
+	const posts = searchParams.search ? postsBySearch : postsAll;
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between gap-3 p-24'>
 			<AddPost />
